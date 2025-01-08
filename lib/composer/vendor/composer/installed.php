8a34a9c96266deda12edb86aa7e92e48a72e8776
Lucas Correia <lucasdevproject@gmail.com>
2025-01-08 20:37:24 -0300

    Update composer

---
+++ lib/composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wef/calendar',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7ce15184738b6437a2b9d75034b6ccd66afa34f1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wef/calendar' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7ce15184738b6437a2b9d75034b6ccd66afa34f1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
