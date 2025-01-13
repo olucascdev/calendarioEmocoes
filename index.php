fb7474daa3b2e5549347b2efff41b6c4a5301bf4
Lucas Correia <lucasdevproject@gmail.com>
2025-01-13 18:54:45 -0300

    Bd
    Puxando do Bd para criar o evento no calendario

--- index.php
+++ index.php
@@ -3,16 +3,19 @@ include("config/config.php");
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calendário Emoções 2025</title>
+    <link rel="stylesheet" href="lib/css/style.css">
+    <script src="lib/js/script.js"></script>
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+    
 </head>
 <body>
-     <?php use Classes\ClassTeste;
 
-            $obj = new ClassTeste();
-?>
+    <div id="calendar"></div>
+ 
 </body>
 </html>
